c2b9af2be807588a54d20012b9cb7d3b8a7bb2bf
zayff1 <muhammadhuudzaifah@gmail.com>
2024-02-23 02:02:28 +0700

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -27,11 +27,11 @@ function changeTheme() {
   if (theme.getAttribute("href") === lightTheme) {
     theme.setAttribute("href", darkTheme);
     themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode 🌙";
+    toast.innerHTML = "Dark Mode";
   } else {
     theme.setAttribute("href", lightTheme);
     themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode ☀️";
+    toast.innerHTML = "Light Mode";
   }
 }
 
